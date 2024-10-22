83b1a14dda68bf3255868f7c5b246f324510596d
ibtissam-laz <lazraf.ibtissam23@gmail.com>
2024-10-22 23:17:14 +0100

    frame

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHight = rowCount * tileSize;
 
+        JFrame frame = new JFrame("Pac Man");
+        frame.setSize(boardWidth, boardHight);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
