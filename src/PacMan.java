238d6c3ffc79654c2835e69cbb7ea79954fe843b
ibtissam-laz <lazraf.ibtissam23@gmail.com>
2024-10-22 23:26:07 +0100

    pac man class

---
+++ src/PacMan.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Random;
+import javax.swing.*;
+
+public class PacMan extends JPanel {
+    private int rowCount = 21;
+    private int columnCount = 19;
+    private int tileSize = 32;
+    private int boardWidth = columnCount * tileSize;
+    private int boardHight = rowCount * tileSize;
+
+    PacMan() {
+
+    }
+}
