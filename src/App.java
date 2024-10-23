e43be2c8268f5f6a875b8d437aaf88876a942317
ibtissam-laz <lazraf.ibtissam23@gmail.com>
2024-10-23 12:54:26 +0100

    images

--- src/App.java
+++ src/App.java
@@ -10,9 +10,14 @@ public class App {
 
         JFrame frame = new JFrame("Pac Man");
         frame.setSize(boardWidth, boardHight);
-        frame.setVisible(true);
+        // frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
--- src/PacMan.java
+++ src/PacMan.java
@@ -11,8 +11,32 @@ public class PacMan extends JPanel {
     private int boardWidth = columnCount * tileSize;
     private int boardHight = rowCount * tileSize;
 
+    private Image wallImage;
+    private Image blueGhostImage;
+    private Image redGhostImage;
+    private Image orangeGhostImage;
+    private Image pinkGhostImage;
+
+    private Image pacmanUpImage;
+    private Image pacmanDownImage;
+    private Image pacmanLeftImage;
+    private Image pacmanRightImage;
+
     PacMan() {
         setPreferredSize(new Dimension(boardWidth, boardHight));
         setBackground(Color.BLACK);
+
+        // Load images
+        wallImage = new ImageIcon(getClass().getResource("./wall.png")).getImage();
+        blueGhostImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
+        orangeGhostImage = new ImageIcon(getClass().getResource("./orangeGhost.png")).getImage();
+        pinkGhostImage = new ImageIcon(getClass().getResource("./pinkGhost.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("./redGhost.png")).getImage();
+
+        pacmanUpImage = new ImageIcon(getClass().getResource("./pacmanUp.png")).getImage();
+        pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
+        pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
+        pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
+
     }
 }
