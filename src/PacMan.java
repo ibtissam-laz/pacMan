96561093a4ee6c5dff54b22e90d009fe95ed87e3
ibtissam-laz <lazraf.ibtissam23@gmail.com>
2024-10-24 23:33:55 +0100

    Map

--- src/PacMan.java
+++ src/PacMan.java
@@ -5,6 +5,28 @@ import java.util.Random;
 import javax.swing.*;
 
 public class PacMan extends JPanel {
+
+    class Block {
+        int x;
+        int y;
+        int width;
+        int hight;
+        Image image;
+
+        int startX;
+        int startY;
+
+        Block(Image image, int x, int y, int width, int hight) {
+            this.image = image;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.hight = hight;
+            this.startX = x;
+            this.startY = y;
+        }
+    }
+
     private int rowCount = 21;
     private int columnCount = 19;
     private int tileSize = 32;
@@ -22,6 +44,37 @@ public class PacMan extends JPanel {
     private Image pacmanLeftImage;
     private Image pacmanRightImage;
 
+    // X = wall, O = skip, P = pac man, ' ' = food
+    // Ghosts: b = blue, o = orange, p = pink, r = red
+    private String[] tileMap = {
+            "XXXXXXXXXXXXXXXXXXX",
+            "X        X        X",
+            "X XX XXX X XXX XX X",
+            "X                 X",
+            "X XX X XXXXX X XX X",
+            "X    X       X    X",
+            "XXXX XXXX XXXX XXXX",
+            "OOOX X       X XOOO",
+            "XXXX X XXrXX X XXXX",
+            "O       bpo       O",
+            "XXXX X XXXXX X XXXX",
+            "OOOX X       X XOOO",
+            "XXXX X XXXXX X XXXX",
+            "X        X        X",
+            "X XX XXX X XXX XX X",
+            "X  X     P     X  X",
+            "XX X X XXXXX X X XX",
+            "X    X   X   X    X",
+            "X XXXXXX X XXXXXX X",
+            "X                 X",
+            "XXXXXXXXXXXXXXXXXXX"
+    };
+
+    HashSet<Block> walls;
+    HashSet<Block> foods;
+    HashSet<Block> ghosts;
+    Block pacman;
+
     PacMan() {
         setPreferredSize(new Dimension(boardWidth, boardHight));
         setBackground(Color.BLACK);
@@ -37,6 +90,13 @@ public class PacMan extends JPanel {
         pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
         pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
         pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
+
+    }
+
+    public void loadMap() {
+        walls = new HashSet<Block>();
+        foods = new HashSet<Block>();
+        ghosts = new HashSet<Block>();
 
     }
 }
